3b28a94d862c264f96233b403bd6bf7d4fa06ad4
Len <lleonard188@gmail.com>
2018-08-25 10:03:11 +0800

    Initial commit

---
+++ venv/AssociationStudy.py
@@ -0,0 +1,18 @@
+class Participant:
+    status = None
+    variants = None
+    microbes = None
+
+
+participants = ["Participant1", "Participant2"]
+
+for x in healthyParticipants
+    #Pair each variant and microbe
+
+for x in unhealthyParticipants
+    #Pair each variant and microbe
+
+#Find which combinations are in unhealthhy but not healthy
+
+
+
